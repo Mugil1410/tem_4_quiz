6535de69336f6c75a5bb28f2cd9bb8292869fb37
Mugil1410 <mugilaananthan@gmail.com>
2026-02-28 19:36:34 +0530

    Add new version files and update sound handling in quiz application

---
+++ v2/data.js
@@ -0,0 +1,17 @@
+const teamMembers = [
+    "Aishwarya Jain D",
+    "Janani A",
+    "Kanmani S",
+    "Kavya Sritha K",
+    "Nivedha NP",
+    "Shivani G"
+];
+
+const projectName = 'Team 4 Quiz';
+
+const wheelSliceColors = [
+  "#ff4d4d","#ff944d","#ffd24d","#d4ff4d","#4dff88",
+  "#4dffd2","#4dd2ff","#4d88ff","#7a4dff","#c44dff",
+  "#ff4df2","#ff4d9e","#ff6b6b","#6bffb3","#6bcfff"
+];
+
